5e34e65f4beff30d911ed917e3ddf10de6e0ef23
theb0imanuu <emmwaniki2004@gmail.com>
2025-12-30 22:04:51 +0300

    Creatd my first fuction in go

--- go/day_1/main.go
+++ go/day_1/main.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
+func add(a int, b int) int {
+	return a + b
+}
+
 //Hello World
 func main() {
 	fmt.Println("Hello World")
+
+	//Call the function
+	a := 10
+	b := 5
+	result := add(a, b)
+	fmt.Println("The sum of ", a, " and ", b, " is ", result)
 }
