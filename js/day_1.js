9da10b7f0d98b9b9141756786e858da705f02c2d
theb0imanuu <emmwaniki2004@gmail.com>
2025-12-30 22:16:03 +0300

    Created a function in Javascript

---
+++ js/day_1.js
@@ -0,0 +1,13 @@
+//Hello world
+
+// 'const' for variable
+const message = "Hello World";
+console.log(message);
+
+// Arrow fucntion
+const add = (a, b) => {
+    return a + b;
+};
+
+const result = add(5, 10);
+console.log("The sum of 5 and 10 is", result);
