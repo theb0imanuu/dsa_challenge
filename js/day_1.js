e2e9e2f196e3b884239af03016f11d68f6123b0e
theb0imanuu <emmwaniki2004@gmail.com>
2025-12-30 22:59:19 +0300

    Did the FizzBuzz problem in JS

--- js/day_1.js
+++ js/day_1.js
@@ -11,3 +11,16 @@ const add = (a, b) => {
 
 const result = add(5, 10);
 console.log("The sum of 5 and 10 is", result);
+
+//FizzBuzz
+for (i = 1; i<= 100; i++){
+    if (i % 15 ===0){
+        console.log("FizzBuzz");
+    }else if (i % 3 ===0){
+        console.log("Fizz");
+    }else if ( i % 5 ===0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
