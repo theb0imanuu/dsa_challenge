0319d0ba1e7c15d344e7f6f7aa3f20e78496a219
theb0imanuu <emmwaniki2004@gmail.com>
2025-12-30 22:31:02 +0300

    Did the FizzBuzz problem in Go

--- go/day_1/main.go
+++ go/day_1/main.go
@@ -6,8 +6,8 @@ func add(a int, b int) int {
 	return a + b
 }
 
-//Hello World
 func main() {
+	//Hello World
 	fmt.Println("Hello World")
 
 	//Call the function
@@ -15,4 +15,17 @@ func main() {
 	b := 5
 	result := add(a, b)
 	fmt.Println("The sum of ", a, " and ", b, " is ", result)
+
+	// FizzBuzz
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
